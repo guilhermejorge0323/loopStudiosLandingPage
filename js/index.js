963b91ffd0e79a53217e9326fa325745c662b437
Guilherme Jorge <guilhermejorge272@gmail.com>
2023-05-02 22:29:47 -0300

    nova versão navbar otimizada

--- js/index.js
+++ js/index.js
@@ -9,18 +9,3 @@
 
 
 
-
-
-
-function mudaNavBar(){
-    const larguraPagina = document.documentElement.clientWidth;
-    const links = document.querySelector('#lista-links');
-    const btnHamburger = document.querySelector('#button-hamburger');
-    if(larguraPagina <= 912){
-        links.classList = 'oculto';
-        btnHamburger.classList = '';
-    }else{
-        links.classList = '';
-        btnHamburger.classList = 'oculto';
-    }
-}
