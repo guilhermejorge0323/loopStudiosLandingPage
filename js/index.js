79e6add2d215cf1f3adc665bd5b1a534d947809a
Guilherme Jorge <guilhermejorge272@gmail.com>
2023-05-05 22:02:29 -0300

    Nav bar pronta :)

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
+const btnHamburger = document.querySelector('#btn-hamburger');
+const btnClose = document.querySelector('#btn-close')
+const lista = document.querySelector('#lista-links');
+
+btnHamburger.addEventListener('click',(e) => {
+    if(!lista.classList.contains('navAtiva')){
+        lista.classList.add('navAtiva');
+    }
+});
 
 
-
-
-
-
-
-
-
-
-
+btnClose.addEventListener('click',(e) => {
+    lista.classList = '';
+});
