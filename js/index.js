5e78d4cb9ccd3c1ec5ce74a8188641fdcd7ea419
Guilherme Jorge <guilhermejorge272@gmail.com>
2023-05-06 23:42:39 -0300

    Conteudo main parte1 pronto

--- js/index.js
+++ js/index.js
@@ -3,12 +3,13 @@ const btnClose = document.querySelector('#btn-close')
 const lista = document.querySelector('#lista-links');
 
 btnHamburger.addEventListener('click',(e) => {
-    if(!lista.classList.contains('navAtiva')){
         lista.classList.add('navAtiva');
-    }
+        document.body.style.overflowY = 'hidden';
+    
 });
 
 
 btnClose.addEventListener('click',(e) => {
     lista.classList = '';
+    document.body.style.overflowY = 'auto';
 });
